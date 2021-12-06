e6d1f1aeb54b1f6e8d851c050dee00206bdcfcda
Agustin <53192876+AgusLaNutria@users.noreply.github.com>
2021-12-05 23:05:37 -0300

    Sin cambios

---
+++ src/main/java/edu/fiuba/algo3/modelo/Edificio/IEdificio.java
@@ -0,0 +1,9 @@
+package edu.fiuba.algo3.modelo.Edificio;
+
+import edu.fiuba.algo3.modelo.Edificio.ComportamientosEdificios.NoVisitado;
+import edu.fiuba.algo3.modelo.Ladron.Ladron;
+
+public interface IEdificio {
+
+    boolean generarEvento(Ladron unLadron);
+}
