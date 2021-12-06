e8f31b96839383d3e0ad688683894e3ad4aa1a57
desencuentro <->
2021-12-06 17:38:02 -0300

    Refactorizando: Usar detalles en vez de parámetros separados (se conserva por compatibilidad)

--- src/main/java/edu/fiuba/algo3/modelo/Ladron/Ladron.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ladron/Ladron.java
@@ -5,25 +5,31 @@ import edu.fiuba.algo3.modelo.Pista.Filtro.IFiltroCiudad;
 import edu.fiuba.algo3.modelo.Pista.IPista;
 import edu.fiuba.algo3.modelo.Policia.Policia;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ladron implements ISospechoso {
-
   private final String nombre;
-  private final String sexo;
-  private final String deporte;
-  private final String cabello;
-  private final String distincion;
-  private final String vehiculo;
-
-  public Ladron(String nombre, String sexo, String deporte, String cabello, String distincion, String vehiculo) {
-
+  private final Map<String,String> detalles;
+  public Ladron(String nombre, Map<String,String> detalles) {
     this.nombre = nombre;
-    this.sexo = sexo;
-    this.deporte = deporte;
-    this.cabello = cabello;
-    this.distincion = distincion;
-    this.vehiculo = vehiculo;
-
+    this.detalles = detalles;
   }
+
+  private static Map<String,String> crearDetalles(String sexo, String deporte, String cabello, String distincion, String vehiculo)
+  {
+    Map<String,String> map = new HashMap<String,String>();
+    map.put("sexo", sexo);
+    map.put("deporte", deporte);
+    map.put("cabello", cabello);
+    map.put("distincion", distincion);
+    map.put("vehiculo", vehiculo);
+    return map;
+  }
+  public Ladron(String nombre, String sexo, String deporte, String cabello, String distincion, String vehiculo) {
+    this(nombre, crearDetalles(sexo,deporte,cabello,distincion,vehiculo));
+  }
+
 
   public IPista pistaAlAzar(Policia policia) {
     return null;
@@ -34,10 +40,15 @@ public class Ladron implements ISospechoso {
     return string.equals(nombre);
   }
 
-  public String mostrarDistincion() {
+  public String getDetalle(String tipo, String porDefecto)
+  {
+    return this.detalles.getOrDefault(tipo,porDefecto);
+  }
 
-    System.out.println("Veo a un sujeto con un " + this.distincion + " dentro del edificio. ");
-    return this.distincion;
+  public String mostrarDistincion() {
+    String distincion = getDetalle("distincion","");
+    System.out.println("Veo a un sujeto con un " + distincion + " dentro del edificio. ");
+    return distincion;
   }
 
   @Override
