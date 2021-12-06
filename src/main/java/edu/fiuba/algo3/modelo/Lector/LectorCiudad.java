fd7334114a6d12a9b3eab18cbc1b28b394decfa4
desencuentro <->
2021-12-06 08:17:37 -0300

    ~leerCiudades(JSONObject) ahora usa interpretarCiudad

--- src/main/java/edu/fiuba/algo3/modelo/Lector/LectorCiudad.java
+++ src/main/java/edu/fiuba/algo3/modelo/Lector/LectorCiudad.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import edu.fiuba.algo3.modelo.Pista.PistaCiudad;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,7 +15,6 @@ import edu.fiuba.algo3.modelo.Ciudad.Ciudad;
 public class LectorCiudad {
 
   InterpretePistaCiudad interprete = new InterpretePistaCiudad();
-  ArrayList<Ciudad> ciudades = new ArrayList<Ciudad>();
 
   public Map<String,Ciudad> leerCiudades() {
     return leerCiudades("/src/main/java/edu/fiuba/algo3/recursos/ciudades.json");
@@ -45,9 +43,9 @@ public class LectorCiudad {
     }
   }
 
-  private JSONArray leerPropiedadComoArray(JSONObject objeto,String propiedad)
+  private JSONArray leerPropiedadComoArray(HashMap objeto,String propiedad)
   {
-    Object valor =objeto.getOrDefault(propiedad,null);
+    Object valor = objeto.getOrDefault(propiedad,null);
     if(!(valor instanceof JSONArray))
     {
       throw new RuntimeException("Error en ciudades, no se encontró array " + propiedad);
@@ -60,38 +58,23 @@ public class LectorCiudad {
     Map<String,Ciudad> dicc = new HashMap<String,Ciudad>();
     int i=0;
     for(Object elemento : ciudades)
+    try
     {
-      String nombre = String.valueOf(i);
-      ArrayList<PistaCiudad> pistas = new ArrayList<>();
-      dicc.put(nombre,new Ciudad(nombre,pistas));
+      if(!(elemento instanceof JSONObject)) {
+        throw new RuntimeException("No es un diccionario.");
+      }
+      Ciudad ciudad = this.interpretarCiudad((JSONObject) elemento);
+      dicc.put(ciudad.getNombre(),ciudad);
       i++;
+    } catch(Exception ex) {
+      ex.printStackTrace();
+      throw new RuntimeException("Ciudad "+i+":" + ex.getMessage());
     }
     return dicc;
-  }
-/*
-  public ArrayList<Ciudad> leerCiudadesDeArchivo(String nombreArchivo) {
-
-    JSONParser parser = new JSONParser();
-
-    try (Reader reader = new FileReader(nombreArchivo)) {
-
-      JSONObject json = (JSONObject) parser.parse(reader);
-      JSONArray ciudadesJson = (JSONArray) json.get("ciudades");
-      Iterator<JSONObject> iterator = ciudadesJson.iterator();
-      while (iterator.hasNext()) {
-        ciudades.add(interpretarCiudad(iterator.next()));
-      }
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    } catch (org.json.simple.parser.ParseException e) {
-      e.printStackTrace();
-    }
-    return ciudades;
   }
 
   private Ciudad interpretarCiudad(JSONObject ciudad) {
     return (new Ciudad((String) ciudad.get("nombre"), interprete.interpretar((JSONArray) ciudad.get("pistas"))));
   }
-*/
+
 }
