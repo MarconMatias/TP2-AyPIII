8606080a9ec4648e76e03872c62ec67a7fca0798
desencuentro <->
2021-12-06 17:51:45 -0300

    +leer2ElementosDaListaCon2LadronesConDetallesCorrectos

--- src/main/test/edu/fiuba/algo3/modelo/Lector/LectorLadronTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Lector/LectorLadronTest.java
@@ -94,4 +94,34 @@ public class LectorLadronTest {
         assertEquals("sexo2", ladrones.get(1).getDetalle("sexo",""));
     }
 
+    @Test
+    public void leer2ElementosDaListaCon2LadronesConDetallesCorrectos()
+    {
+        String fuente = "{\"ladrones\":["+
+                "{\"nombre\":\"Ladron1\",\"sexo\":\"sexo1\",\"deporte\":\"basket\",\"cabello\":\"rojo\",\"distincion\":\"anillo\",\"vehiculo\":\"Ferrari\",},"+
+                "{\"nombre\":\"Ladron2\",\"sexo\":\"sexo2\",\"deporte\":\"handball\",\"cabello\":\"azul\",\"distincion\":\"piercing\",\"vehiculo\":\"Unicornio\",},"+
+                "]}";
+        LectorLadron lector = new LectorLadron();
+
+        JSONObject entrada;
+        try
+        {
+            entrada = (JSONObject) (new JSONParser()).parse(fuente);
+        }
+        catch(ParseException ex) {
+            throw new RuntimeException("Error al parsear test: "+ex.toString());
+        }
+        List<Ladron> ladrones = lector.leerLadrones(entrada);
+        assertEquals("sexo1", ladrones.get(0).getDetalle("sexo",""));
+        assertEquals("basket", ladrones.get(0).getDetalle("deporte",""));
+        assertEquals("rojo", ladrones.get(0).getDetalle("cabello",""));
+        assertEquals("anillo", ladrones.get(0).getDetalle("distincion",""));
+        assertEquals("Ferrari", ladrones.get(0).getDetalle("vehiculo",""));
+        assertEquals("sexo2", ladrones.get(1).getDetalle("sexo",""));
+        assertEquals("handball", ladrones.get(1).getDetalle("deporte",""));
+        assertEquals("azul", ladrones.get(1).getDetalle("cabello",""));
+        assertEquals("piercing", ladrones.get(1).getDetalle("distincion",""));
+        assertEquals("Unicornio", ladrones.get(1).getDetalle("vehiculo",""));
+    }
+
 }
