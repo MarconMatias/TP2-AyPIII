0c82006d1f7e05caf2cc2aa1513814daf81253fd
desencuentro <->
2021-12-06 06:43:52 -0300

    +arrayCon3ElementosDaListaCon3PistasDeTipoCorrecto

--- src/main/test/edu/fiuba/algo3/modelo/Lector/InterpretePistaCiudadTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Lector/InterpretePistaCiudadTest.java
@@ -8,6 +8,8 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
 import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class InterpretePistaCiudadTest {
@@ -37,5 +39,27 @@ public class InterpretePistaCiudadTest {
         }
         ArrayList<PistaCiudad> salida = interprete.interpretar(entrada);
         assertEquals(3,salida.size());
+    }
+
+    @Test
+    public void arrayCon3ElementosDaListaCon3PistasDeTipoCorrecto()
+    {
+        InterpretePistaCiudad interprete = new InterpretePistaCiudad();
+        String fuente = "[{\"tipo\":\"Bandera\",\"pista\":\"Verde, blanca y roja\",\"dificultad\":0},";
+        fuente += "{\"tipo\":\"Comida\",\"pista\":\"Tacos\",\"dificultad\":0},";
+        fuente += "{\"tipo\":\"Paisajes\",\"pista\":\"Monte Popocatepetl\",\"dificultad\":0}]";
+        JSONArray entrada;
+        try
+        {
+            entrada = (JSONArray) (new JSONParser()).parse(fuente);
+        }
+        catch(ParseException ex) {
+            throw new RuntimeException("Error al parsear test: "+ex.toString());
+        }
+        ArrayList<PistaCiudad> salida = interprete.interpretar(entrada);
+        boolean primeroEsBandera = salida.get(0).esDeUnTipoDe(List.of("Bandera"));
+        assertTrue(primeroEsBandera);
+        assertTrue(salida.get(1).esDeUnTipoDe(List.of("Comida")));
+        assertTrue(salida.get(2).esDeUnTipoDe(List.of("Paisajes")));
     }
 }
