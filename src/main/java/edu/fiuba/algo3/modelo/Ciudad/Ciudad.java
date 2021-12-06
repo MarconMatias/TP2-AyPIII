5849804c88c7a445cd4a0ae318aca99ca10697ca
desencuentro <->
2021-12-06 02:04:04 -0300

    cantidadDeVisitas→EstadoVisitasCiudad (patrón State), visitar devuelve CiudadVisitada, +edificiosAlAzar +pistaAlAzar

--- src/main/java/edu/fiuba/algo3/modelo/Ciudad/Ciudad.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ciudad/Ciudad.java
@@ -1,25 +1,54 @@
 package edu.fiuba.algo3.modelo.Ciudad;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
-import edu.fiuba.algo3.modelo.Pista.PistaCiudad;
+import edu.fiuba.algo3.modelo.Ciudad.EstadoVisitas.EstadoVisitasCiudad;
+import edu.fiuba.algo3.modelo.Edificio.Edificio;
+import edu.fiuba.algo3.modelo.Edificio.TipoEdificio.*;
+import edu.fiuba.algo3.modelo.Juego.Calendario;
+import edu.fiuba.algo3.modelo.Pista.*;
+import edu.fiuba.algo3.modelo.Pista.Filtro.IFiltroCiudad;
+import edu.fiuba.algo3.modelo.Pista.NivelPista.NivelPista;
+import edu.fiuba.algo3.modelo.Pista.NivelPista.PistaDificil;
+import edu.fiuba.algo3.modelo.Pista.NivelPista.PistaFacil;
+import edu.fiuba.algo3.modelo.Pista.NivelPista.PistaMedia;
+import edu.fiuba.algo3.modelo.Policia.*;
 
 public class Ciudad {
 
     private final String nombre;
     private final ArrayList<PistaCiudad> pistas;
-    private int cantidadDeVisitas = 0;
+    private EstadoVisitasCiudad visitas = new EstadoVisitasCiudad();
 
     public Ciudad(String nombre, ArrayList<PistaCiudad> pistas) {
         this.nombre = nombre;
         this.pistas = pistas;
     }
 
-    public void visitar() {
-        //
+    public CiudadVisitada visitar(Policia policia, Calendario calendario) {
+        return new CiudadVisitada(this,policia,calendario);
     }
 
     public boolean esLaCiudad(String nombre) {
         return this.nombre.equals(nombre);
+    }
+
+    private final static ArrayList<Supplier<Edificio>> factories = new ArrayList<>(
+            List.of(
+                    () -> new Edificio(new Banco()),
+                    () -> new Edificio(new Bolsa()),
+                    () -> new Edificio(new Banco())
+            )
+    );
+
+    public List<Edificio> edificiosAlAzar(int max) {
+        Collections.shuffle(factories);
+        return factories.stream().limit(max).map(factory -> factory.get()).collect(Collectors.toList());
+    }
+
+    public IPista pistaAlAzar(Policia policia, IFiltroCiudad filtroCiudad) {
+         return filtroCiudad.filtrarPistas(pistas).stream().findFirst().orElse(new PistaCiudad("Desconocida","No sabría darte alguna pista"));
     }
 }
