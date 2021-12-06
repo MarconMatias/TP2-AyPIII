1a73095f96633fcac0954a5288e04395377d08b2
desencuentro <->
2021-12-06 04:12:53 -0300

    ~filtrarPistas: Devuelve lista, no uno al azar.

--- src/main/java/edu/fiuba/algo3/modelo/Pista/NivelPista/NivelPista.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pista/NivelPista/NivelPista.java
@@ -1,23 +1,22 @@
 package edu.fiuba.algo3.modelo.Pista.NivelPista;
 
 import edu.fiuba.algo3.modelo.Pista.IPista;
+import edu.fiuba.algo3.modelo.Pista.PistaCiudad;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 
 public abstract class NivelPista {
 
-    public IPista filtrarPistas(ArrayList<IPista> pistas) {
+    public ArrayList<IPista> filtrarPistas(Collection<PistaCiudad> pistas) {
 
         ArrayList<IPista> pistasFiltradas = new ArrayList<IPista>();
         for (IPista p : pistas) {
             p.agregarAListaSiEsNivel(pistasFiltradas,this);
         }
-
-        Random numeroRandom = new Random();
-        int indiceRandom = numeroRandom.nextInt(pistasFiltradas.size());
-        return pistasFiltradas.get((indiceRandom));
-    }
+        return pistasFiltradas;
+   }
 
     public abstract boolean esEquivalente(NivelPista nivel);
 }
