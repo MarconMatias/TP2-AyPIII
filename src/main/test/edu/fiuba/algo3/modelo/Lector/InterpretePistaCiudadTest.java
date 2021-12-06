1f3e95d46719b5075d251b0f1b49e08d62cc7107
desencuentro <->
2021-12-06 07:07:09 -0300

    arrayCon3ElementosDaListaCon3PistasConNivelCorrecto

--- src/main/test/edu/fiuba/algo3/modelo/Lector/InterpretePistaCiudadTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Lector/InterpretePistaCiudadTest.java
@@ -1,9 +1,9 @@
 package edu.fiuba.algo3.modelo.Lector;
 
+import edu.fiuba.algo3.modelo.Pista.IPista;
+import edu.fiuba.algo3.modelo.Pista.NivelPista.*;
 import edu.fiuba.algo3.modelo.Pista.PistaCiudad;
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
@@ -85,5 +85,43 @@ public class InterpretePistaCiudadTest {
         assertEquals("Verde, blanca y roja", salida.get(0).getValor());
         assertEquals("Tacos", salida.get(1).getValor());
         assertEquals("Monte Popocatepetl", salida.get(2).getValor());
+    }
+
+    @Test
+    public void arrayCon3ElementosDaListaCon3PistasConNivelCorrecto()
+    {
+        InterpretePistaCiudad interprete = new InterpretePistaCiudad();
+        ArrayList<IPista> faciles = new ArrayList<IPista>();
+        ArrayList<IPista> medias = new ArrayList<IPista>();
+        ArrayList<IPista> dificiles = new ArrayList<IPista>();
+        NivelPista facil = new PistaFacil();
+        NivelPista media = new PistaMedia();
+        NivelPista dificil = new PistaDificil();
+        JSONArray entrada = null;
+
+        String fuente = "[{\"tipo\":\"Bandera\",\"pista\":\"Verde, blanca y roja\",\"dificultad\":1},";
+        fuente += "{\"tipo\":\"Comida\",\"pista\":\"Tacos\",\"dificultad\":2},";
+        fuente += "{\"tipo\":\"Paisajes\",\"pista\":\"Monte Popocatepetl\",\"dificultad\":0}]";
+
+        try
+        {
+            entrada = (JSONArray) (new JSONParser()).parse(fuente);
+        }
+        catch(ParseException ex) {
+            throw new RuntimeException("Error al parsear test: "+ex.toString());
+        }
+        ArrayList<PistaCiudad> salida = interprete.interpretar(entrada);
+        for(IPista pista:salida)
+        {
+            pista.agregarAListaSiEsNivel(faciles,facil);
+            pista.agregarAListaSiEsNivel(medias,media);
+            pista.agregarAListaSiEsNivel(dificiles,dificil);
+        }
+        assertEquals(1, faciles.size());
+        assertEquals(1, medias.size());
+        assertEquals(1, dificiles.size());
+        assertEquals("Verde, blanca y roja", ((PistaCiudad) medias.get(0)).getValor());
+        assertEquals("Tacos", ((PistaCiudad) dificiles.get(0)).getValor());
+        assertEquals("Monte Popocatepetl", ((PistaCiudad) faciles.get(0)).getValor());
     }
 }
