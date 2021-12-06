5ace74e12a403aabf637aa94119a311d4d7218e2
desencuentro <->
2021-12-06 01:40:26 -0300

    Comportamiento adicional para edificios

--- src/main/java/edu/fiuba/algo3/modelo/Edificio/IComportamientoEdificio.java
+++ src/main/java/edu/fiuba/algo3/modelo/Edificio/IComportamientoEdificio.java
@@ -1,8 +1,11 @@
 package edu.fiuba.algo3.modelo.Edificio;
 
+import edu.fiuba.algo3.modelo.Juego.Calendario;
 import edu.fiuba.algo3.modelo.Ladron.Ladron;
+import edu.fiuba.algo3.modelo.Policia.Policia;
 
 public interface IComportamientoEdificio {
 
     IComportamientoEdificio lanzarEvento(Ladron unLadron);
+    void visitar(Edificio edificio, Policia policia, Calendario cal);
 }
