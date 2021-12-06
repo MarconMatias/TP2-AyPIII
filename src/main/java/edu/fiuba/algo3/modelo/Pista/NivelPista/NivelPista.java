53d27329bc205e595de878fd37dfc6703c7fbee1
desencuentro <->
2021-12-06 01:24:13 -0300

    -propiedad nivel, +abstract esEquivalente()

--- src/main/java/edu/fiuba/algo3/modelo/Pista/NivelPista/NivelPista.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pista/NivelPista/NivelPista.java
@@ -3,26 +3,25 @@ package edu.fiuba.algo3.modelo.Pista.NivelPista;
 import edu.fiuba.algo3.modelo.Pista.IPista;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public abstract class NivelPista {
-
-    private final String nivel;
-
-    public NivelPista(String nivel) {
-        this.nivel = nivel;
-    }
 
     public IPista filtrarPistas(ArrayList<IPista> pistas){
 
         ArrayList<IPista> pistasFiltradas = new ArrayList<IPista>();
-        for (IPista p: pistas){
-
-            p.agregarAListaSiEsNivel(pistasFiltradas,this.nivel);
+        for (IPista p: pistas) {
+            p.agregarAListaSiEsNivel(pistasFiltradas,this);
         }
 
         Random numeroRandom = new Random();
         int indiceRandom = numeroRandom.nextInt(pistasFiltradas.size());
         return pistasFiltradas.get((indiceRandom));
     }
+
+    public abstract boolean esEquivalente(NivelPista nivel);
 }
