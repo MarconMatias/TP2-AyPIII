b534e5569ba97ecde53398bcf8950a0397f7265d
desencuentro <->
2021-12-06 03:28:03 -0300

    Vuelvo a remover nivel como propiedad y en constructor y definir esEquivalente (abstracto)

--- src/main/java/edu/fiuba/algo3/modelo/Pista/NivelPista/NivelPista.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pista/NivelPista/NivelPista.java
@@ -7,22 +7,17 @@ import java.util.Random;
 
 public abstract class NivelPista {
 
-    private final String nivel;
-
-    public NivelPista(String nivel) {
-        this.nivel = nivel;
-    }
-
     public IPista filtrarPistas(ArrayList<IPista> pistas) {
 
         ArrayList<IPista> pistasFiltradas = new ArrayList<IPista>();
         for (IPista p : pistas) {
-
-            // p.agregarAListaSiEsNivel(pistasFiltradas,this.nivel);
+            p.agregarAListaSiEsNivel(pistasFiltradas,this);
         }
 
         Random numeroRandom = new Random();
         int indiceRandom = numeroRandom.nextInt(pistasFiltradas.size());
         return pistasFiltradas.get((indiceRandom));
     }
+
+    public abstract boolean esEquivalente(NivelPista nivel);
 }
