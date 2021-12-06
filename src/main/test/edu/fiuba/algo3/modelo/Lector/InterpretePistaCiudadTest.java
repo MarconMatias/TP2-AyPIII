a0618a1a36ae3d0a0ce724208f9935130c167bc6
desencuentro <->
2021-12-06 05:42:11 -0300

    Creado con primer test: interpretarArrayVacioDaListaVacia

--- src/main/test/edu/fiuba/algo3/modelo/Lector/InterpretePistaCiudadTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Lector/InterpretePistaCiudadTest.java
@@ -1,2 +1,18 @@
-package edu.fiuba.algo3.modelo.Lector;public class InterpretePistaCiudadTest {
+package edu.fiuba.algo3.modelo.Lector;
+
+import edu.fiuba.algo3.modelo.Pista.PistaCiudad;
+import org.json.simple.JSONArray;
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
+public class InterpretePistaCiudadTest {
+    @Test
+    public void interpretarArrayVacioDaListaVacia()
+    {
+        InterpretePistaCiudad interprete = new InterpretePistaCiudad();
+        JSONArray entrada = new JSONArray();
+        ArrayList<PistaCiudad> salida = interprete.interpretar(entrada);
+        assertEquals(0,salida.size());
+    }
 }
