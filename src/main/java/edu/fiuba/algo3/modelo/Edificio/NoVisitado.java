208dcaaf711b8564f4f1706223ad27657f829b1d
desencuentro <->
2021-12-06 03:20:12 -0300

    Revierto eliminado, temporalmente, para resolver conflicto de fusión

---
+++ src/main/java/edu/fiuba/algo3/modelo/Edificio/NoVisitado.java
@@ -0,0 +1,17 @@
+package edu.fiuba.algo3.modelo.Edificio;
+
+import edu.fiuba.algo3.modelo.Ladron.Ladron;
+
+public class NoVisitado implements IComportamientoEdificio {
+
+    public NoVisitado() {
+
+    }
+
+    @Override
+    public IComportamientoEdificio lanzarEvento(Ladron unLadron) {
+
+        // unLadron.mostrarSeña();
+        return new Visitado();
+    }
+}
