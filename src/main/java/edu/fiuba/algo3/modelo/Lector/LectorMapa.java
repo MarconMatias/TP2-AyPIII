242c1a4cde1f2ffa7a0d4dc60278a022d50e0585
desencuentro <->
2021-12-08 18:18:48 -0300

    ~leerMapa, +interpretarOrigen, cargan todos los origen->destino sin distancia

--- src/main/java/edu/fiuba/algo3/modelo/Lector/LectorMapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Lector/LectorMapa.java
@@ -1,13 +1,35 @@
 package edu.fiuba.algo3.modelo.Lector;
 
 import edu.fiuba.algo3.modelo.Juego.Mapa;
-import edu.fiuba.algo3.modelo.Ladron.Ladron;
 import org.json.simple.JSONObject;
 
+import java.util.Map;
+
 public class LectorMapa {
+    Mapa mapa = new Mapa();
 
     public Mapa leerMapa(JSONObject entrada) {
-        Mapa mapa = new Mapa();
+        LectorJson lectorJson = new LectorJson();
+        Map mapaJson = lectorJson.leerPropiedadComo(Map.class,entrada,"mapa");
+        for (Object clave:mapaJson.keySet()) {
+            if(!(clave instanceof String))
+            {
+                throw new RuntimeException("Hay un origen que no es String, es "+clave.getClass().getName()+": "+clave.toString());
+            }
+            String origen = (String) clave;
+            Map destinosJson = lectorJson.leerPropiedadComo(Map.class,mapaJson,origen);
+            interpretarOrigen(origen,destinosJson);
+        }
         return mapa;
+    }
+
+    private void interpretarOrigen(String origen, Map destinosJson) {
+        for (Object clave:destinosJson.keySet()) {
+            if (!(clave instanceof String)) {
+                throw new RuntimeException("En " + origen + " hay un destino que no es String, es " + clave.getClass().getName() + ": " + clave.toString());
+            }
+            String destino = (String) clave;
+            mapa.agregarConexion(origen, destino, 0);
+        }
     }
 }
