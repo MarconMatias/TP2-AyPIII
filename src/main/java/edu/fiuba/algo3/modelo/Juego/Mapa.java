77235df069dca7119703fbcf6731ea5676ad16c2
desencuentro <->
2021-12-08 17:11:37 -0300

    Pasa 3 tests

--- src/main/java/edu/fiuba/algo3/modelo/Juego/Mapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego/Mapa.java
@@ -3,9 +3,39 @@ package edu.fiuba.algo3.modelo.Juego;
 import edu.fiuba.algo3.modelo.Ciudad.CiudadVisitada;
 import edu.fiuba.algo3.modelo.Policia.Policia;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Mapa {
+
+    private Map<String, Map<String, Integer>> origenes = new HashMap<String,Map<String,Integer>>();
 
     public CiudadVisitada viajarHacia(Policia policiaDeLaMision, Calendario calendario, String nombreDeLaCiudadDestino) {
         return null;
+    }
+
+    public void viajar(Policia policia, String origen, String destino, Calendario calendario) {
+        if(!origenes.containsKey(origen))
+        {
+            throw new RuntimeException("El origen no existe en el mapa.");
+        }
+        Map<String,Integer> destinos = origenes.get(origen);
+        if(!destinos.containsKey(destino))
+        {
+            throw new RuntimeException("El destino no existe en el mapa con ese origen.");
+        }
+        Integer distancia = destinos.get(destino);
+        policia.viajar(distancia, calendario);
+    }
+
+    public void agregarConexion(String origen, String destino, int distanciaKm) {
+        Map<String,Integer> destinos;
+        if(origenes.containsKey(origen)) {
+            destinos = origenes.get(origen);
+        } else {
+            destinos = new HashMap<>();
+            origenes.put(origen,destinos);
+        }
+        destinos.put(destino,distanciaKm);
     }
 }
