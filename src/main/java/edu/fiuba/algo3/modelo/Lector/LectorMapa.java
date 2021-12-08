f22725733267f288cd2599f97c21f2f6cb2f38b6
desencuentro <->
2021-12-08 18:00:53 -0300

    +leerMapa devuelve mapa vacío

--- src/main/java/edu/fiuba/algo3/modelo/Lector/LectorMapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Lector/LectorMapa.java
@@ -1,4 +1,13 @@
 package edu.fiuba.algo3.modelo.Lector;
 
-public class LectorMapa extends LectorLadron {
+import edu.fiuba.algo3.modelo.Juego.Mapa;
+import edu.fiuba.algo3.modelo.Ladron.Ladron;
+import org.json.simple.JSONObject;
+
+public class LectorMapa {
+
+    public Mapa leerMapa(JSONObject entrada) {
+        Mapa mapa = new Mapa();
+        return mapa;
+    }
 }
