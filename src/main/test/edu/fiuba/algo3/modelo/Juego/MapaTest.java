182a49f5b5f0a60bd1289bf83c730e9d2b95796c
desencuentro <->
2021-12-09 03:10:05 -0300

    ~Policia: rango→arrestos, Mapa::viajar: String origen→Ciudad origen, ~Edificio::visitar:-Calendario (ahora en Policía)

--- src/main/test/edu/fiuba/algo3/modelo/Juego/MapaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/Juego/MapaTest.java
@@ -1,25 +1,27 @@
 package edu.fiuba.algo3.modelo.Juego;
 
+import edu.fiuba.algo3.modelo.Ciudad.Ciudad;
 import edu.fiuba.algo3.modelo.Policia.Policia;
-import edu.fiuba.algo3.modelo.Policia.RangoPolicia.RangoPolicia;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class MapaTest {
+
     @Test
     public void irDeCiudadACiudadConMapaVacioFalla()
     {
-        Mapa mapa = new Mapa();
-        Policia policia = new Policia(new RangoPolicia(), "Matute");
+        Mapa mapa = new Mapa(new HashMap<String, Ciudad>());
+        Policia policia = new Policia("Matute",0);
         Calendario calendario = new Calendario();
         try
         {
-            mapa.viajar(policia, "Eregion", "Rivendel", calendario);
+            mapa.viajar(policia, mapa.getCiudadPorNombre("Eregion"), "Rivendel");
             Assert.fail("Debe lanzar una excepción.");
         } catch (RuntimeException ex) {
         }
@@ -28,24 +30,24 @@ public class MapaTest {
     @Test
     public void irDeCiudadExistenteADestinoExistentePasa()
     {
-        Mapa mapa = new Mapa();
-        Policia policia = new Policia(new RangoPolicia(), "Matute");
+        Mapa mapa = new Mapa(new HashMap<String,Ciudad>());
+        Policia policia = new Policia("Matute",0);
         Calendario calendario = new Calendario();
         mapa.agregarConexion("Atenas","Bangkok",7917);
-        mapa.viajar(policia, "Atenas", "Bangkok", calendario);
+        mapa.viajar(policia, mapa.getCiudadPorNombre("Atenas"), "Bangkok");
     }
 
     @Test
     public void irDeCiudadExistenteACiudadExistenteNoDestinoFalla()
     {
-        Mapa mapa = new Mapa();
-        Policia policia = new Policia(new RangoPolicia(), "Matute");
+        Mapa mapa = new Mapa(new HashMap<String,Ciudad>());
+        Policia policia = new Policia("Matute",0);
         Calendario calendario = new Calendario();
         mapa.agregarConexion("Atenas","Bangkok",7917);
         mapa.agregarConexion("Bagdád","Colombo",4681);
         try
         {
-            mapa.viajar(policia, "Atenas", "Colombo", calendario);
+            mapa.viajar(policia, mapa.getCiudadPorNombre("Atenas"), "Colombo");
             Assert.fail("Debe lanzar una excepción.");
         } catch (RuntimeException ex) {
         }
@@ -54,28 +56,28 @@ public class MapaTest {
     @Test
     public void viajarDeBamakoAMoroniEntre3DestinosLleva6270Km()
     {
-        Mapa mapa = new Mapa();
+        Mapa mapa = new Mapa(new HashMap<String,Ciudad>());
         Policia policia = mock(Policia.class);
         Calendario calendario = new Calendario();
         mapa.agregarConexion("Bamako","Montreal",7113);
         mapa.agregarConexion("Bamako","Moroni",6270);
         mapa.agregarConexion("Bamako","Tokyo",13657);
 
-        mapa.viajar(policia, "Bamako", "Moroni", calendario);
-        verify(policia).viajar(6270,calendario);
+        mapa.viajar(policia, mapa.getCiudadPorNombre("Bamako"), "Moroni");
+        verify(policia).viajar(6270);
     }
 
     @Test
     public void obtenerOrigenesConMapaVacioEstaVacio()
     {
-        Mapa mapa = new Mapa();
+        Mapa mapa = new Mapa(new HashMap<String,Ciudad>());
         assertEquals(0, mapa.getOrigenes().size());
     }
 
     @Test
     public void getOrigenesConMapa3OrigenesDevuelve3MismosOrigenes()
     {
-        Mapa mapa = new Mapa();
+        Mapa mapa = new Mapa(new HashMap<String,Ciudad>());
         mapa.agregarConexion("Bamako","Montreal",7113);
         mapa.agregarConexion("Bamako","Moroni",6270);
         mapa.agregarConexion("Bamako","Tokyo",13657);
