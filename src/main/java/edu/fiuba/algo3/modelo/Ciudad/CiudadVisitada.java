09ef748754d4c23e27925def9b183642ea31d3dd
desencuentro <->
2021-12-09 02:48:57 -0300

    State visitada/no visitada: +uso ICiudadVisitada, actualizo métodos

--- src/main/java/edu/fiuba/algo3/modelo/Ciudad/CiudadVisitada.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ciudad/CiudadVisitada.java
@@ -1,49 +1,40 @@
 package edu.fiuba.algo3.modelo.Ciudad;
 
 import edu.fiuba.algo3.modelo.Ciudad.EstadoVisitas.EstadoVisitasCiudad;
-import edu.fiuba.algo3.modelo.Edificio.TipoEdificio.ITipoEdificio;
+import edu.fiuba.algo3.modelo.Edificio.Edificio;
 import edu.fiuba.algo3.modelo.Juego.Calendario;
-import edu.fiuba.algo3.modelo.Ladron.Ladron;
+import edu.fiuba.algo3.modelo.Ladron.ISospechoso;
 import edu.fiuba.algo3.modelo.Policia.Policia;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class CiudadVisitada implements IComportamientoCiudad {
-    /*
+public class CiudadVisitada implements ICiudadVisitada {
     private final int cantidadEdificios = 3;
-
-    private final Ciudad ciudad;
-    private final Calendario cal;
-    private final Policia policia;
     private final EstadoVisitasCiudad visitas = new EstadoVisitasCiudad();
-    private final List<ITipoEdificio> edificios;
-    private Ladron ladron = null;
-    private Ciudad destino = null;
+    private final List<Edificio> edificios;
+    private final Policia policia;
 
-    public CiudadVisitada(Ciudad ciudad, Policia policia, Calendario cal)
+    public CiudadVisitada(Ciudad estaCiudad, Policia policia, ISospechoso sospechoso, Ciudad ciudadSiguiente)
     {
-        this.ciudad = ciudad;
+        this.edificios = estaCiudad.edificiosAlAzar(3);
         this.policia = policia;
-        this.cal = cal;
-
-        //edificios = ciudad.edificiosAlAzar(3);
-        throw new RuntimeException("Revisar");
-    }
-
-    public void visitadoPorLadron(Ladron ladron, Ciudad destino)
-    {
-        for(ITipoEdificio edificio : edificios) {
-            edificio.visitadoPorLadron(ladron,destino);
+        for(Edificio edificio : edificios) {
+            edificio.visitadoPorLadron(sospechoso,ciudadSiguiente);
         }
     }
 
-    public void visitarEdificio(ITipoEdificio edificio)
+    @Override
+    public List<Edificio> obtenerEdificios() {
+        return edificios;
+    }
+
+    @Override
+    public void visitarEdificio(Edificio edificio)
     {
+        /** \todo Debería estar en la lista edificios. */
         int demora = visitas.demoraEdificio();
         visitas.siguiente();
-        cal.avanzarHoras(demora);
-        edificio.visitar(policia,cal);
+        policia.avanzarHoras(demora);
+        edificio.visitar(policia);
     }
-     */
 }
