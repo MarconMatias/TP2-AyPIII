ae3711eaca44ca3632306b97ecaea6ac0357d92d
desencuentro <->
2021-12-09 18:49:38 -0300

    Interfaz para observadores de acciones en calendario

--- src/main/java/edu/fiuba/algo3/modelo/Juego/IObservadorAcciones.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego/IObservadorAcciones.java
@@ -1,2 +1,7 @@
-package edu.fiuba.algo3.modelo.Juego;public interface IObservadorAcciones {
+package edu.fiuba.algo3.modelo.Juego;
+
+import edu.fiuba.algo3.modelo.Acciones.IAccion;
+
+public interface IObservadorAcciones {
+    void accionRealizada(IAccion accion);
 }
