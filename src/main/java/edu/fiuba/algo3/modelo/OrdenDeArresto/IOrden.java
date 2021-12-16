225d92cb8be58a6829916618042ea718316ab888
desencuentro <->
2021-12-16 19:06:35 -0300

    +enfrentar

--- src/main/java/edu/fiuba/algo3/modelo/OrdenDeArresto/IOrden.java
+++ src/main/java/edu/fiuba/algo3/modelo/OrdenDeArresto/IOrden.java
@@ -1,2 +1,8 @@
-package edu.fiuba.algo3.modelo.OrdenDeArresto;public interface IOrden {
+package edu.fiuba.algo3.modelo.OrdenDeArresto;
+
+import edu.fiuba.algo3.modelo.Ladron.Ladron;
+import edu.fiuba.algo3.modelo.Policia.Policia;
+
+public interface IOrden {
+    public void enfrentar(Policia policia, Ladron ladron);
 }
