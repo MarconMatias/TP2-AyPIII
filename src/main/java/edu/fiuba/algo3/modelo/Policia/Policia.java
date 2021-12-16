09f23fdba856e5c63029ea7c63168b5f9a9e38b9
Agustin <53192876+AgusLaNutria@users.noreply.github.com>
2021-12-15 21:31:26 -0300

    Commit pa que Bren pullee

--- src/main/java/edu/fiuba/algo3/modelo/Policia/Policia.java
+++ src/main/java/edu/fiuba/algo3/modelo/Policia/Policia.java
@@ -58,10 +58,11 @@ public class Policia {
     }
 
 
-    public void viajar(int distancia)
+    public int viajar(int distancia)
     {
 
         calendario.avanzarHoras(rango.devolverTiempoDeViaje(distancia));
+        return rango.devolverTiempoDeViaje(distancia); //el return sirve para los tests
 
     }
     public ArrayList<IPista> filtrarPistas(Collection<IPista> pistas){
---
+++ src/main/test/edu/fiuba/algo3/modelo/Policia/TestPolicia.java
@@ -0,0 +1,15 @@
+package edu.fiuba.algo3.modelo.Policia;
+
+import edu.fiuba.algo3.modelo.Juego.Calendario;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class TestPolicia {
+
+    @Test
+    public void policiaNovatoViaja1800KmsYTarda2Horas(){
+
+        Policia policia = new Policia("Agus",0, new Calendario());
+        Assert.assertTrue(policia.viajar(1800) == 2);
+    }
+}
