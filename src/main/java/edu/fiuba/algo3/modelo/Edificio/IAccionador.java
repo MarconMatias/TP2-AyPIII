ecceaf03095165156a424efe8199445b0a4d4bf8
desencuentro <->
2021-12-16 19:11:11 -0300

    IComportamientoEdificio→IAccionador

--- src/main/java/edu/fiuba/algo3/modelo/Edificio/IAccionador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Edificio/IAccionador.java
@@ -1,12 +1,11 @@
 package edu.fiuba.algo3.modelo.Edificio;
 
 import edu.fiuba.algo3.modelo.Edificio.TipoEdificio.ITipoEdificio;
-import edu.fiuba.algo3.modelo.Juego.Calendario;
 import edu.fiuba.algo3.modelo.Ladron.Ladron;
 import edu.fiuba.algo3.modelo.Policia.Policia;
 
-public interface IComportamientoEdificio {
+public interface IAccionador {
 
-    IComportamientoEdificio lanzarEvento(Ladron unLadron);
-    void visitar(ITipoEdificio edificio, Policia policia);
+    IAccionador lanzarEvento(Ladron unLadron);
+    void visitar(Edificio edificio, Policia policia);
 }
