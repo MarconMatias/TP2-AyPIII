b6f52a3b12619a31612ab2392b7ee194bddff577
desencuentro <->
2021-12-16 19:08:00 -0300

    Estrategia de Orden/SinOrden (de arresto) para ganar/perder partida.

--- src/main/java/edu/fiuba/algo3/modelo/OrdenDeArresto/Estrategia/Ganar.java
+++ src/main/java/edu/fiuba/algo3/modelo/OrdenDeArresto/Estrategia/Ganar.java
@@ -1,2 +1,12 @@
-package edu.fiuba.algo3.modelo.OrdenDeArresto.Estrategia;public class Ganar {
+package edu.fiuba.algo3.modelo.OrdenDeArresto.Estrategia;
+
+import edu.fiuba.algo3.modelo.Ladron.Ladron;
+import edu.fiuba.algo3.modelo.Policia.Policia;
+
+public class Ganar implements IEstrategiaOrden {
+    @Override
+    public void realizar(Policia policia, Ladron ladron) {
+        policia.agregarArresto();
+        // Llamar a los eventos
+    }
 }
--- src/main/java/edu/fiuba/algo3/modelo/OrdenDeArresto/Estrategia/Perder.java
+++ src/main/java/edu/fiuba/algo3/modelo/OrdenDeArresto/Estrategia/Perder.java
@@ -1,2 +1,18 @@
-package edu.fiuba.algo3.modelo.OrdenDeArresto.Estrategia;public class Perder {
+package edu.fiuba.algo3.modelo.OrdenDeArresto.Estrategia;
+
+import edu.fiuba.algo3.modelo.Ladron.Ladron;
+import edu.fiuba.algo3.modelo.Policia.Policia;
+
+public class Perder implements IEstrategiaOrden {
+
+    private final String explicacion;
+
+    public Perder(String explicacion) {
+        this.explicacion = explicacion;
+    }
+
+    @Override
+    public void realizar(Policia policia, Ladron ladron) {
+        // Notificar perdió y texto?
+    }
 }
