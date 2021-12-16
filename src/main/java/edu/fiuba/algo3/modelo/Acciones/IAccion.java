ba6908402b348378a9c5a64a9e3aad651192a957
desencuentro <->
2021-12-16 19:05:39 -0300

    +setPolicia, +realizar

--- src/main/java/edu/fiuba/algo3/modelo/Acciones/IAccion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Acciones/IAccion.java
@@ -1,9 +1,14 @@
 package edu.fiuba.algo3.modelo.Acciones;
 
 import edu.fiuba.algo3.modelo.Juego.Calendario;
+import edu.fiuba.algo3.modelo.Policia.Policia;
 
 public interface IAccion {
     public String getNombreAccion();
 
     void avanzarCalendario(Calendario calendario);
+
+    void setPolicia(Policia policia);
+
+    void realizar();
 }
