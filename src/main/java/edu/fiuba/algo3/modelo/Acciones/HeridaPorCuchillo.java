07d6c6d0577a095c86df1faaa8c9d2672914e204
desencuentro <->
2021-12-16 19:15:30 -0300

    +setPolicia +realizar

--- src/main/java/edu/fiuba/algo3/modelo/Acciones/HeridaPorCuchillo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Acciones/HeridaPorCuchillo.java
@@ -7,10 +7,12 @@ import edu.fiuba.algo3.modelo.Policia.Policia;
 public class HeridaPorCuchillo implements IAccion, IComportamientoAccion{
     private int demoraAccion;
     private AccionCuchilloUnica estado;
+    private Policia policia;
+
     @Override
-    public void reaccion(Calendario calendario, Policia policia /* Por las dudas */) {
-        demoraAccion = estado.demoraEdificio();
-        calendario.avanzarHoras(demoraAccion);
+    public void reaccion(Calendario calendario, Policia policia) {
+        //demoraAccion = estado.demoraEdificio();
+        //calendario.avanzarHoras(demoraAccion);
         // Mati - No se bien que haria el policia como reaccion a la accion
     }
 
@@ -21,7 +23,16 @@ public class HeridaPorCuchillo implements IAccion, IComportamientoAccion{
 
     @Override
     public void avanzarCalendario(Calendario calendario) {
-        /** Falta comportamiento por cantidad */
-        calendario.avanzarHoras(2);
+        policia.avanzarHorasCuchillada(calendario);
+    }
+
+    @Override
+    public void setPolicia(Policia policia) {
+        this.policia = policia;
+    }
+
+    @Override
+    public void realizar() {
+       policia.recibirCuchillada();
     }
 }
--- src/main/java/edu/fiuba/algo3/modelo/Acciones/HeridaPorDisparo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Acciones/HeridaPorDisparo.java
@@ -3,12 +3,31 @@ package edu.fiuba.algo3.modelo.Acciones;
 import edu.fiuba.algo3.modelo.Juego.Calendario;
 import edu.fiuba.algo3.modelo.Policia.Policia;
 
-public class HeridaPorDisparo implements IComportamientoAccion{
+public class HeridaPorDisparo implements IComportamientoAccion, IAccion {
 
     @Override
     public void reaccion(Calendario calendario, Policia policia) {
-        calendario.avanzarHoras(4);
+        //calendario.avanzarHoras(4);
         // Mati - No se bien que haria el policia como reaccion a la accion
     }
-    
+
+    @Override
+    public String getNombreAccion() {
+        return "Disparo";
+    }
+
+    @Override
+    public void avanzarCalendario(Calendario calendario) {
+        calendario.avanzarHoras(4);
+    }
+
+    @Override
+    public void setPolicia(Policia policia) {
+        /** No debe hacer nada porque no interactúa. **/
+    }
+
+    @Override
+    public void realizar() {
+        /** No debe hacer nada porque no cambia el esado. **/
+    }
 }
