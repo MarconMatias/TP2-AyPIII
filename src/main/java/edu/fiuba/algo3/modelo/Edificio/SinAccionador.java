85ceaccb59307196ba5e0f1537e06d459becdd2b
desencuentro <->
2021-12-16 19:13:12 -0300

    SinComportamiento → SinAccionador

--- src/main/java/edu/fiuba/algo3/modelo/Edificio/SinAccionador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Edificio/SinAccionador.java
@@ -4,14 +4,14 @@ import edu.fiuba.algo3.modelo.Edificio.TipoEdificio.ITipoEdificio;
 import edu.fiuba.algo3.modelo.Ladron.Ladron;
 import edu.fiuba.algo3.modelo.Policia.Policia;
 
-public class SinComportamiento implements IAccionador {
+public class SinAccionador implements IAccionador {
     @Override
     public IAccionador lanzarEvento(Ladron unLadron) {
         return null;
     }
 
     @Override
-    public void visitar(ITipoEdificio edificio, Policia policia) {
+    public void visitar(Edificio edificio, Policia policia) {
         // No tiene que hacer nada
     }
 }
