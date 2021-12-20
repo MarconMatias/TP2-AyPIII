dc69d94246149229729dde6e582ff9ea03f825bb
desencuentro <->
2021-12-20 10:23:17 -0300

    +escalaProperty

--- src/main/java/edu/fiuba/algo3/componentes/Imagen/Imagen.java
+++ src/main/java/edu/fiuba/algo3/componentes/Imagen/Imagen.java
@@ -1,27 +1,38 @@
 package edu.fiuba.algo3.componentes.Imagen;
 
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.image.ImageView;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Scale;
+
 import java.io.File;
 import java.net.MalformedURLException;
 
 public class Imagen extends ImageView {
     private final DoubleProperty angulo = new SimpleDoubleProperty(0.0);
+    private final DoubleProperty escala = new SimpleDoubleProperty(1.0);
 
     public Imagen(String path) {
         super(path);
+        setFitWidth(getImage().getWidth());
+        setFitHeight(getImage().getHeight());
         Rotate rotacionEfecto = new Rotate();
         rotacionEfecto.pivotXProperty().bind(fitWidthProperty().divide(2));
         rotacionEfecto.pivotYProperty().bind(fitHeightProperty().divide(2));
         rotacionEfecto.angleProperty().bind(angulo);
-        this.getTransforms().setAll(rotacionEfecto);
+        Scale escaladoEfecto = new Scale();
+        escaladoEfecto.xProperty().bind(escala);
+        escaladoEfecto.yProperty().bind(escala);
+        this.getTransforms().setAll(rotacionEfecto, escaladoEfecto);
     }
 
     public DoubleProperty anguloProperty() {
         return angulo;
+    }
+
+    public DoubleProperty escalaProperty() {
+        return escala;
     }
 
     public final double getAngulo() {
