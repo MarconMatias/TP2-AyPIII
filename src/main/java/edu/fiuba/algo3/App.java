1007a813752dd097ecc29fcdcd986a8436fe54d7
desencuentro <->
2021-12-20 06:55:32 -0300

    Muestra splash con porcentaje de carga, y luego selección de agente de policía

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,9 +1,17 @@
 package edu.fiuba.algo3;
 
+import edu.fiuba.algo3.componentes.Cuaderno.Cuaderno;
+import edu.fiuba.algo3.componentes.FakeLoader.FakeService;
+import edu.fiuba.algo3.componentes.bindings.CargandoBinding;
+import edu.fiuba.algo3.controlador.Splash.SplashControlador;
+import edu.fiuba.algo3.modelo.Juego.Juego;
+import edu.fiuba.algo3.vista.Policias;
+import edu.fiuba.algo3.vista.Splash;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.collections.ObservableList;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.Event;
+import javafx.scene.Group;
 import javafx.stage.Stage;
 
 /**
@@ -11,17 +19,61 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    Juego juego = new Juego();
+    Stage stage;
+
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        this.stage = stage;
+        startSplash(stage);
     }
 
+    ControladorStage controlador;
+
+    private void startSplash(Stage stage) {
+        juego = new Juego();
+        FakeService lector = new FakeService(5);
+        CargandoBinding tituloBinding = new CargandoBinding(lector.progressProperty(),
+                "AlgoThief — ¡Listo!",
+                "AlgoThief — Cargando… %.2f%%");
+        stage.titleProperty().bind(tituloBinding);
+
+        Splash splash = new Splash();
+        Group root = new Group(splash);
+        controlador = new ControladorStage(stage, root);
+        //Rectangle2D scr = controlador.getDimensionPantalla();
+        controlador.start();
+        stage.sizeToScene();
+        stage.centerOnScreen();
+        splash.progresoProperty().bind(lector.progressProperty());
+        lector.start();
+        stage.show();
+        SplashControlador splashControlador = new SplashControlador(splash);
+        lector.setOnSucceeded(ev -> {
+            stage.titleProperty().unbind();
+            splashControlador.bind(this::mostrarPolicias);
+        });git
+        lector.setOnFailed(this::onSplashFailed);
+    }
+
+    private void onSplashExitoso(SplashControlador splashControlador) {
+
+    }
+
+    private void onSplashFailed(WorkerStateEvent workerStateEvent) {
+        /** \todo Mínimo, mostrar un mensaje de error. **/
+        /** \todo Mejorar: reintentar (vuelve a llamar a startSplash). **/
+    }
+
+    private void mostrarPolicias(Event event) {
+        Group principal = new Policias(juego.getPolicias());
+        Group root = new Group(principal);
+        controlador = new ControladorStage(stage, root);
+        controlador.start();
+        stage.setTitle("AlgoThief — Elija el agente para iniciar una misión");
+        stage.sizeToScene();
+        stage.centerOnScreen();
+    }
     public static void main(String[] args) {
         launch();
     }
