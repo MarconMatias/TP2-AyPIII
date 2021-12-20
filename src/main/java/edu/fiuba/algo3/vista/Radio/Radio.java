e0a0fb58c73f50ff78a3514c68fe19f4aaf61c2b
desencuentro <->
2021-12-20 11:48:53 -0300

    Cambiar imagen según estado

--- src/main/java/edu/fiuba/algo3/vista/Radio/Radio.java
+++ src/main/java/edu/fiuba/algo3/vista/Radio/Radio.java
@@ -1,9 +1,9 @@
-package edu.fiuba.algo3.componentes.Radio;
+package edu.fiuba.algo3.vista.Radio;
 
 import edu.fiuba.algo3.componentes.Imagen.Imagen;
-import javafx.beans.binding.DoubleBinding;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
+import edu.fiuba.algo3.controlador.Radio.RadioControlador;
+import javafx.beans.binding.*;
+import javafx.beans.property.*;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
@@ -14,15 +14,16 @@ import java.util.stream.Stream;
 
 public class Radio extends Imagen {
 
-    private BooleanProperty encendido = new SimpleBooleanProperty(true);
+    private RadioControlador controlador;
+    private final BooleanProperty encendido = new SimpleBooleanProperty(true);
     private static List<Image> imgs = generarImage();
 
     private static List<Image> generarImage() {
         return Stream.of(
                 "radio/radio_1160_inactivo_desel.png",
                 "radio/radio_1160_inactivo_sel.png",
-                "radio/radio_1160_activo_sel.png",
-                "radio/radio_1160_activo_desel.png")
+                "radio/radio_1160_activo_desel.png",
+                "radio/radio_1160_activo_sel.png")
                 .map(Radio::urlDesdeRecursos).map(Image::new).collect(Collectors.toList());
     }
 
@@ -45,10 +46,31 @@ public class Radio extends Imagen {
         brilloHover.brightnessProperty().bind(hoverBinding);
         brilloHover.hueProperty().bind(hoverBinding);
         super.setOnMouseClicked(this::mouseClicked);
+        setFocusTraversable(true);
+
+        imageProperty().bind(new ObjectBinding<Image>() {
+            {
+                this.bind(focusedProperty(),encendido);
+            }
+            @Override
+            protected Image computeValue() {
+                int numeroImagen = (encendido.get()?2:0) + (focusedProperty().get()?1:0);
+                return imgs.get(numeroImagen);
+            }
+        });
+    }
+
+    public Radio(RadioControlador controlador) {
+        this();
+        this.controlador = controlador;
     }
 
     private void mouseClicked(MouseEvent mouseEvent) {
-        requestFocus();
-        System.out.println();
+        if(!isFocused()) {
+            requestFocus();
+            mouseEvent.consume();
+            return;
+        }
     }
+
 }
