0d73ebd9fddb75f445cd8d300400e92d73d22c5a
desencuentro <->
2021-12-20 06:53:11 -0300

    Mapamundi de fondo con avion para reutilizar

---
+++ src/main/java/edu/fiuba/algo3/componentes/Mapamundi/Mapamundi.java
@@ -0,0 +1,35 @@
+package edu.fiuba.algo3.componentes.Mapamundi;
+
+import edu.fiuba.algo3.componentes.Imagen.Avion;
+import edu.fiuba.algo3.componentes.RelativoAImagen.RelativoAImagen;
+import edu.fiuba.algo3.componentes.bindings.Point2DBindingXY;
+import edu.fiuba.algo3.componentes.bindings.SimplePoint2DBinding;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.geometry.Point2D;
+
+public class Mapamundi extends RelativoAImagen {
+    private final Avion avion = new Avion();
+    private final SimplePoint2DBinding cooordenadasAvion = new SimplePoint2DBinding(0.5,0.5);
+
+    public Mapamundi() {
+        super("src/main/java/edu/fiuba/algo3/recursos/MapaAlgoThief_3840.jpeg");
+        agregar(avion, cooordenadasAvion.xProperty(), cooordenadasAvion.yProperty());
+    }
+
+    public DoubleProperty anguloAvionProperty() {
+        return avion.anguloProperty();
+    }
+
+    public SimplePoint2DBinding cooordenadasAvionProperty() {
+        return cooordenadasAvion;
+    }
+
+    public Point2D getCooordenadasAvion() {
+        return cooordenadasAvion.get();
+    }
+
+    public void setCoordenadasAvion(Point2D nueva) {
+        cooordenadasAvion.set(nueva);
+    }
+}
