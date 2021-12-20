12375c2f84bd07de5159d9fc37fa9548fcf7c3fb
desencuentro <->
2021-12-20 10:25:05 -0300

    setFit*→escalaProperty, angulo 45→80

--- src/main/java/edu/fiuba/algo3/componentes/Radio/Radio.java
+++ src/main/java/edu/fiuba/algo3/componentes/Radio/Radio.java
@@ -6,8 +6,7 @@ public class Radio extends Imagen {
 
     public Radio() {
         super(urlDesdeRecursos("radio/radio_1160_inactivo_desel.png"));
-        setFitWidth(getFitWidth()/3);
-        setFitHeight(getFitHeight()/3);
-        anguloProperty().set(45);
+        escalaProperty().set(0.5);
+        anguloProperty().set(80);
     }
 }
