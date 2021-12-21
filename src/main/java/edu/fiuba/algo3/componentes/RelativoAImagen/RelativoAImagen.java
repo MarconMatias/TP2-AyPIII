d7a513a4de80775c3e719621aa3650c367751e96
desencuentro <->
2021-12-21 13:32:44 -0300

    AgregarTrayecto con Point2D

--- src/main/java/edu/fiuba/algo3/componentes/RelativoAImagen/RelativoAImagen.java
+++ src/main/java/edu/fiuba/algo3/componentes/RelativoAImagen/RelativoAImagen.java
@@ -28,10 +28,10 @@ public class RelativoAImagen extends Group {
         imageView.setFitWidth(w);
         imageView.setFitHeight(h);
         imageView.setViewOrder(0);
-        imageView.viewportProperty().set(new Rectangle2D(0,0,fondo.getWidth(),fondo.getHeight()));
+        imageView.viewportProperty().set(new Rectangle2D(0, 0, fondo.getWidth(), fondo.getHeight()));
         this.getChildren().add(imageView);
 
-        Rectangle clip = new Rectangle(0,0,10,10);
+        Rectangle clip = new Rectangle(0, 0, 10, 10);
         clip.widthProperty().bind(imagen.widthProperty());
         clip.heightProperty().bind(imagen.heightProperty());
         setClip(clip);
@@ -44,11 +44,12 @@ public class RelativoAImagen extends Group {
     public Point2DBindingXY nuevoRelativoConAbsoluto(Point2DBindingXY absoluto) {
         DoubleBinding x = absoluto.xProperty().divide(fondo.getWidth());
         DoubleBinding y = absoluto.yProperty().divide(fondo.getHeight());
-        return new Point2DBindingXY(x,y);
+        return new Point2DBindingXY(x, y);
     }
 
     /**
      * Genera un binding para las coordenadas absolutas del valor relativo dado por centroRelativo.
+     *
      * @param centroRelativo Coordenadas relativas, entre 0.0 y 1.0, del centro del punto.
      * @param totalContenido Ancho (absoluto) de la imagen a centrar (0.0 para no centrar).
      * @return Un binding para las coordenadas ABSOLUTAS del punto relativo.
@@ -59,6 +60,7 @@ public class RelativoAImagen extends Group {
 
     /**
      * Genera un binding para las coordenadas absolutas del valor relativo dado.
+     *
      * @param posicion Coordenadas relativas, entre 0.0 y 1.0, del centro del punto.
      * @return Un binding para las coordenadas ABSOLUTAS del punto relativo dado.
      */
@@ -68,6 +70,7 @@ public class RelativoAImagen extends Group {
 
     /**
      * Genera un binding para las coordenadas absolutas del valor relativo dado.
+     *
      * @param posicion Coordenadas relativas, entre 0.0 y 1.0, del centro del punto.
      * @return Un binding para las coordenadas ABSOLUTAS del punto relativo dado.
      */
@@ -77,6 +80,7 @@ public class RelativoAImagen extends Group {
 
     /**
      * Genera un binding para las coordenadas absolutas del valor relativo dado por centroRelativo.
+     *
      * @param centroRelativo Coordenadas relativas, entre 0.0 y 1.0, del centro del punto.
      * @param totalContenido Alto (absoluto) de la imagen a centrar (0.0 para no centrar).
      * @return Un binding para las coordenadas ABSOLUTAS del punto relativo.
@@ -87,6 +91,7 @@ public class RelativoAImagen extends Group {
 
     /**
      * Genera un binding para las coordenadas absolutas del valor relativo dado.
+     *
      * @param posicion Coordenadas relativas, entre 0.0 y 1.0, del centro del punto.
      * @return Un binding para las coordenadas ABSOLUTAS del punto relativo dado.
      */
@@ -96,22 +101,24 @@ public class RelativoAImagen extends Group {
 
     /**
      * Genera un binding para las coordenadas absolutas del valor relativo dado.
+     *
      * @param relativo Coordenadas relativas, entre 0.0 y 1.0 cada componente, del centro del punto.
      * @return Un binding para las coordenadas ABSOLUTAS del punto relativo dado.
      */
     private Point2DBindingXY nuevoRelativo(Point2DBindingXY relativo) {
         DoubleBinding x = nuevoXRelativo(relativo.xProperty());
         DoubleBinding y = nuevoYRelativo(relativo.yProperty());
-        return new SimplePoint2DBinding(x,y);
+        return new SimplePoint2DBinding(x, y);
     }
 
     /**
      * Agrega el nodo dado centrado en las coordenadas relativas dadas usando el ancho y alto dado.
-     * @param nodo Nodo a agregar.
-     * @param centroX Coordenadas relativas, entre 0.0 y 1.0 del componente X del centro del nodo.
-     * @param centroY Coordenadas relativas, entre 0.0 y 1.0 del componente Y del centro del nodo.
+     *
+     * @param nodo      Nodo a agregar.
+     * @param centroX   Coordenadas relativas, entre 0.0 y 1.0 del componente X del centro del nodo.
+     * @param centroY   Coordenadas relativas, entre 0.0 y 1.0 del componente Y del centro del nodo.
      * @param anchoReal Ancho (absoluto) de la imagen a centrar (0.0 para no centrar). Suele usarse widthProperty.
-     * @param altoReal Alto (absoluto) de la imagen a centrar (0.0 para no centrar). Suele usarse heightProperty.
+     * @param altoReal  Alto (absoluto) de la imagen a centrar (0.0 para no centrar). Suele usarse heightProperty.
      * @return
      */
     public Node agregar(Node nodo, DoubleExpression centroX, DoubleExpression centroY, DoubleExpression anchoReal, DoubleExpression altoReal) {
@@ -123,7 +130,8 @@ public class RelativoAImagen extends Group {
 
     /**
      * Agrega la imagen dada centrada en las coordenadas relativas dadas.
-     * @param nodo Imagen  a agregar.
+     *
+     * @param nodo    Imagen  a agregar.
      * @param centroX Coordenadas relativas, entre 0.0 y 1.0 del componente X del centro de la imagen.
      * @param centroY Coordenadas relativas, entre 0.0 y 1.0 del componente Y del centro de la imagen.
      * @return
@@ -135,19 +143,21 @@ public class RelativoAImagen extends Group {
 
     /**
      * Agrega la imagen dada centrada en las coordenadas relativas dadas.
-     * @param nodo Imagen  a agregar.
+     *
+     * @param nodo    Imagen  a agregar.
      * @param centroX Coordenadas relativas, entre 0.0 y 1.0 del componente X del centro de la imagen.
      * @param centroY Coordenadas relativas, entre 0.0 y 1.0 del componente Y del centro de la imagen.
      * @return
      */
     public Imagen agregar(Imagen nodo, double centroX, double centroY) {
-        agregar( nodo, new ReadOnlyDoubleWrapper(centroX), new ReadOnlyDoubleWrapper(centroY));
+        agregar(nodo, new ReadOnlyDoubleWrapper(centroX), new ReadOnlyDoubleWrapper(centroY));
         return nodo;
     }
 
     /**
      * Agrega la región dada centrada en las coordenadas relativas dadas.
-     * @param nodo Región a agregar.
+     *
+     * @param nodo    Región a agregar.
      * @param centroX Coordenadas relativas, entre 0.0 y 1.0 del componente X del centro de la imagen.
      * @param centroY Coordenadas relativas, entre 0.0 y 1.0 del componente Y del centro de la imagen.
      * @return
@@ -159,7 +169,8 @@ public class RelativoAImagen extends Group {
 
     /**
      * Agrega la región dada centrada en las coordenadas relativas dadas.
-     * @param nodo Región a agregar.
+     *
+     * @param nodo    Región a agregar.
      * @param centroX Coordenadas relativas, entre 0.0 y 1.0 del componente X del centro de la imagen.
      * @param centroY Coordenadas relativas, entre 0.0 y 1.0 del componente Y del centro de la imagen.
      * @return
@@ -172,6 +183,10 @@ public class RelativoAImagen extends Group {
         Trayecto trayecto = new Trayecto(nuevoRelativo(desdeRelativo), nuevoRelativo(hastaRelativo));
         this.getChildren().add(trayecto);
         return trayecto;
+    }
+
+    public Trayecto agregarTrayecto(Point2D desdeRelativo, Point2D hastaRelativo) {
+        return agregarTrayecto(new SimplePoint2DBinding(desdeRelativo), new SimplePoint2DBinding(hastaRelativo));
     }
 
     public Point2D absolutoConRelativo(Point2D relativo) {
