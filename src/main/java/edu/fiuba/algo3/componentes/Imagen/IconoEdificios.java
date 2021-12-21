9422b3de423d6dcc107296951429d1336bbb7505
desencuentro <->
2021-12-21 17:07:19 -0300

    Para el plano con los edificios (usa imagen de Mapamundi por ahora)

---
+++ src/main/java/edu/fiuba/algo3/componentes/Imagen/IconoEdificios.java
@@ -0,0 +1,24 @@
+package edu.fiuba.algo3.componentes.Imagen;
+
+import javafx.scene.image.Image;
+
+public class IconoEdificios extends ImagenSeleccionable {
+    /** \todo Poner nombres de imágenes correctas cuando estén disponibles. **/
+    private final static Image sel = new Image(urlDesdeRecursos("Mapa/Mapita_sel.png"));
+    private final static Image desel = new Image(urlDesdeRecursos("Mapa/Mapita_desel.png"));
+
+    public IconoEdificios() {
+        super(sel,desel);
+    }
+
+    /**
+     * Crea una imagen seleccionable a modo de ícono, con el ancho dado.
+     *
+     * @param ancho Ancho en píxeles.
+     */
+    public IconoEdificios(double ancho) {
+        this();
+        setWidth(ancho);
+    }
+
+}
