b01679f0acef2e1788b6161ecce1448095ed92f6
desencuentro <->
2021-12-21 10:49:36 -0300

    Click o Enter, abrirLibro() [no hace nada]

--- src/main/java/edu/fiuba/algo3/controlador/Mapa/MapaDestinosControlador.java
+++ src/main/java/edu/fiuba/algo3/controlador/Mapa/MapaDestinosControlador.java
@@ -3,6 +3,9 @@ package edu.fiuba.algo3.controlador.Mapa;
 import edu.fiuba.algo3.ControladorStage;
 import edu.fiuba.algo3.modelo.Juego.Juego;
 import edu.fiuba.algo3.modelo.Juego.Mision;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class MapaDestinosControlador {
     private final Juego juego;
@@ -13,5 +16,23 @@ public class MapaDestinosControlador {
         this.juego = juego;
         this.mision = mision;
         this.controladorStage = controladorStage;
+    }
+
+    public void libritoClicked(MouseEvent ev) {
+        if(ev.isConsumed()) {
+            return;
+        }
+        abrirLibro();
+    }
+
+    public void libritoKeyPressed(KeyEvent ev) {
+        if(ev.isConsumed() || (KeyCode.ENTER != ev.getCode())) {
+            return;
+        }
+        abrirLibro();
+    }
+
+    private void abrirLibro() {
+        /** \todo **/
     }
 }
