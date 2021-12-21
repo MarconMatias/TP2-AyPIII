1e0d5d910fe8ec5ed4d3010e346d5e87fd6e66e0
desencuentro <->
2021-12-21 20:34:00 -0300

    Movido desde DestinoCiudad

--- src/main/java/edu/fiuba/algo3/componentes/Imagen/Destino.java
+++ src/main/java/edu/fiuba/algo3/componentes/Imagen/Destino.java
@@ -1,7 +1,14 @@
 package edu.fiuba.algo3.componentes.Imagen;
 
+import edu.fiuba.algo3.componentes.Trayecto.Trayecto;
 import edu.fiuba.algo3.componentes.bindings.BooleanDoubleBinding;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.Event;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class Destino extends ImagenSeleccionable {
     private static final double ordenSeleccionado = -20;
@@ -9,6 +16,8 @@ public class Destino extends ImagenSeleccionable {
     private static final Image sel = new Image(urlDesdeRecursos("Ciudad/DestinoCiudad_640_sel.png"));
     private static final Image desel = new Image(urlDesdeRecursos("Ciudad/DestinoCiudad_640_desel.png"));
     private String nombre;
+    private Trayecto trayecto;
+    private ObjectProperty<Event> elegido = new SimpleObjectProperty<Event>(null);
 
     public Destino(Image imageSel, Image imageDesel, String nombre) {
         super(imageSel, imageDesel);
@@ -17,10 +26,37 @@ public class Destino extends ImagenSeleccionable {
                 ordenDeseleccionado);
         viewOrderProperty().bind(orden);
         this.nombre = nombre;
+        addEventHandler(MouseEvent.MOUSE_CLICKED, this::mouseClicked);
+        addEventHandler(KeyEvent.KEY_PRESSED, this::keyPressed);
     }
 
     public Destino(String nombre) {
         this(getImageSel(), getImageDesel(), nombre);
+    }
+
+    public Double anguloParaProgreso(double valorProgreso) {
+        if(null == trayecto) {
+            return null;
+        }
+        return trayecto.anguloEnProgreso(valorProgreso);
+    }
+
+    public ObjectProperty<Event> elegidoProperty() {
+        return elegido;
+    }
+
+    private void elegir(Event event) {
+        elegido.set(event);
+        event.isConsumed();
+    }
+
+    private void keyPressed(KeyEvent event) {
+        if(event.isConsumed()) {
+            return;
+        }
+        if(KeyCode.ENTER==event.getCode()) {
+            elegir(event);
+        }
     }
 
     protected static Image getImageDesel() {
@@ -34,4 +70,18 @@ public class Destino extends ImagenSeleccionable {
     public String getNombre() {
         return nombre;
     }
+
+    private void mouseClicked(MouseEvent event) {
+        if(event.isConsumed()) {
+            return;
+        }
+        if(1 == event.getButton().ordinal()) {
+            elegir(event);
+        }
+    }
+
+    public void setTrayecto(Trayecto trayecto) {
+        this.trayecto = trayecto;
+    }
+
 }
