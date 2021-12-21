b24099b41a490ae9f4792d7421ad0b304338787f
desencuentro <->
2021-12-21 20:54:20 -0300

    +destinoElegido=visitarEdificio [falta mostrar resultado]

--- src/main/java/edu/fiuba/algo3/controlador/Edificio/EdificiosControlador.java
+++ src/main/java/edu/fiuba/algo3/controlador/Edificio/EdificiosControlador.java
@@ -2,9 +2,11 @@ package edu.fiuba.algo3.controlador.Edificio;
 
 import edu.fiuba.algo3.ControladorStage;
 import edu.fiuba.algo3.controlador.Ciudad.LibroCiudadControlador;
+import edu.fiuba.algo3.modelo.Edificio.Edificio;
 import edu.fiuba.algo3.modelo.Juego.Juego;
 import edu.fiuba.algo3.modelo.Juego.Mision;
 import edu.fiuba.algo3.vista.Ciudad.LibroCiudad;
+import edu.fiuba.algo3.vista.Edificio.DestinoEdificio;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.KeyCode;
@@ -49,4 +51,9 @@ public class EdificiosControlador {
         }
     }
 
+    public void destinoElegido(DestinoEdificio destino) {
+        Edificio edificio = destino.getEdificio();
+        System.out.println("Visitando edificio "+edificio.getNombre());
+        mision.visitarEdificio(edificio);
+    }
 }
