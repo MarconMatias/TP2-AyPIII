4529a8d42159ff87c68c33c2e006481e8e26a2ab
desencuentro <->
2021-12-22 19:53:12 -0300

    Mover walkman a Pantalla

--- src/main/java/edu/fiuba/algo3/componentes/RelativoAImagen/RelativoAImagen.java
+++ src/main/java/edu/fiuba/algo3/componentes/RelativoAImagen/RelativoAImagen.java
@@ -4,9 +4,6 @@ import edu.fiuba.algo3.componentes.Imagen.Imagen;
 import edu.fiuba.algo3.componentes.Trayecto.Trayecto;
 import edu.fiuba.algo3.componentes.bindings.Point2DBindingXY;
 import edu.fiuba.algo3.componentes.bindings.SimplePoint2DBinding;
-import edu.fiuba.algo3.controlador.Radio.RadioControlador;
-import edu.fiuba.algo3.modelo.Radio.Radio;
-import edu.fiuba.algo3.vista.Radio.Walkman;
 import javafx.beans.binding.DoubleBinding;
 import javafx.beans.binding.DoubleExpression;
 import javafx.beans.property.ReadOnlyDoubleWrapper;
@@ -14,8 +11,6 @@ import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
@@ -24,7 +19,6 @@ import javafx.scene.shape.Rectangle;
 public class RelativoAImagen extends Group {
     private final Image fondo;
     private final ImageView imageView;
-    private Walkman walkman = null;
 
     public RelativoAImagen(Image imagen) {
         this.fondo = imagen;
@@ -45,10 +39,6 @@ public class RelativoAImagen extends Group {
 
     public RelativoAImagen(String path) {
         this(new Image(Imagen.urlDesdePath(path)));
-    }
-
-    protected Walkman getWalkman() {
-        return walkman;
     }
 
     public Point2DBindingXY nuevoRelativoConAbsoluto(Point2DBindingXY absoluto) {
@@ -203,24 +193,5 @@ public class RelativoAImagen extends Group {
         double x = relativo.getX() * fondo.getWidth();
         double y = relativo.getY() * fondo.getHeight();
         return new Point2D(x,y);
-    }
-
-    protected void setRadio(Radio radio) {
-        if(null != walkman) {
-            getChildren().remove(walkman);
-            /* \todo liberar walkman anterior para que se desuscriba. */
-        }
-        if(null == radio) {
-            walkman = null;
-            return;
-        }
-        try {
-            walkman = new Walkman(new RadioControlador(radio));
-            agregar((Imagen) walkman, 0.026, 0.285);
-        } catch(Exception ex) {
-            ex.printStackTrace();
-            Alert alert = new Alert(Alert.AlertType.WARNING, "Error al crear la radio, es posible que la escuche pero no pueda controlarla.", ButtonType.OK);
-            alert.showAndWait();
-        }
     }
 }
--- src/main/java/edu/fiuba/algo3/vista/Juego/Pantalla.java
+++ src/main/java/edu/fiuba/algo3/vista/Juego/Pantalla.java
@@ -1,14 +1,53 @@
 package edu.fiuba.algo3.vista.Juego;
 
+import edu.fiuba.algo3.componentes.Imagen.Imagen;
 import edu.fiuba.algo3.componentes.RelativoAImagen.RelativoAImagen;
+import edu.fiuba.algo3.controlador.Radio.RadioControlador;
+import edu.fiuba.algo3.modelo.Radio.Radio;
+import edu.fiuba.algo3.vista.Radio.Walkman;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 
 public class Pantalla extends RelativoAImagen {
+    private Walkman walkman = null;
+
     public Pantalla(Image imagen) {
         super(imagen);
     }
 
     public Pantalla(String path) {
         super(path);
+    }
+
+    /**
+     * Devuelve el controlador de walkman.
+      * @return La instancia o null si no hay.
+     */
+    protected Walkman getWalkman() {
+        return walkman;
+    }
+
+    /**
+     * Configura la radio para mostrar un Walkman.
+     * @param radio Radio a asociar al walkman, o null para ocultarlo.
+     */
+    protected void setRadio(Radio radio) {
+        if(null != walkman) {
+            getChildren().remove(walkman);
+            /* \todo liberar walkman anterior para que se desuscriba. */
+        }
+        if(null == radio) {
+            walkman = null;
+            return;
+        }
+        try {
+            walkman = new Walkman(new RadioControlador(radio));
+            agregar((Imagen) walkman, 0.026, 0.285);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Error al crear la radio, es posible que la escuche pero no pueda controlarla.", ButtonType.OK);
+            alert.showAndWait();
+        }
     }
 }
