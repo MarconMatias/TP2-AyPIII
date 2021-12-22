195f26dad555e150fd35e08206bb00be86811baa
bhaberkon <brendamicaela.haberkon@mercadolibre.com>
2021-12-22 17:48:47 -0300

    policias y sospechosos

--- src/main/java/edu/fiuba/algo3/modelo/Juego/Mision.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego/Mision.java
@@ -10,6 +10,8 @@ import edu.fiuba.algo3.modelo.Juego.EstadoMision.EstadoMision;
 import edu.fiuba.algo3.modelo.Ladron.Ladron;
 import edu.fiuba.algo3.modelo.Policia.Policia;
 import edu.fiuba.algo3.modelo.Ruta.Ruta;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.util.List;
 import java.util.Random;
@@ -29,20 +31,20 @@ public class Mision {
 
     /**
      * Inicia una misión con todos los parámetros dados.
-     * @param policia El policía que realiza la investigación.
-     * @param itemRobado El elemento que fue robado.
-     * @param ladron El ladron que realizó el robo.
-     * @param rutaLadron La ruta que tomó el ladrón.
+     * 
+     * @param policia       El policía que realiza la investigación.
+     * @param itemRobado    El elemento que fue robado.
+     * @param ladron        El ladron que realizó el robo.
+     * @param rutaLadron    La ruta que tomó el ladrón.
      * @param ciudadInicial El nombre de la ciudad en la que está el policía.
-     * @param computadora La computadora de Interpol.
-     * @param mapa El mapa de ciudades.
-     * @param calendario El calendario de tiempo del juego.
-     * @param random El generador de números aleatorios.
+     * @param computadora   La computadora de Interpol.
+     * @param mapa          El mapa de ciudades.
+     * @param calendario    El calendario de tiempo del juego.
+     * @param random        El generador de números aleatorios.
      */
     public Mision(Policia policia, Item itemRobado, Ladron ladron, List<Ciudad> rutaLadron, String ciudadInicial,
-                  Computadora computadora, Mapa mapa,
-                  Calendario calendario, Random random)
-    {
+            Computadora computadora, Mapa mapa,
+            Calendario calendario, Random random) {
         this.estadoMision = new EstadoMision();
         this.policia = policia;
         policia.iniciarMision(calendario);
@@ -69,73 +71,80 @@ public class Mision {
     }
 
     /**
-     * Crea una misión para un policía, con un item y ladrón al azar, ruta al azar y ciudad según el ítem.
-     * Para generar el ítem, ladrón y ruta usa un nuevo generador y un nuevo calendario.
+     * Crea una misión para un policía, con un item y ladrón al azar, ruta al azar y
+     * ciudad según el ítem.
+     * Para generar el ítem, ladrón y ruta usa un nuevo generador y un nuevo
+     * calendario.
      * Esta sobrecarga está diseñada para ser usada "por defecto".
-     * @param policia El policía que realiza la investigación.
-     * @param items Listado de items para elegir uno como robado.
+     * 
+     * @param policia  El policía que realiza la investigación.
+     * @param items    Listado de items para elegir uno como robado.
      * @param ladrones Listado de ladrones para elegir uno.
-     * @param mapa El mapa de ciudades.
+     * @param mapa     El mapa de ciudades.
      */
     public Mision(Policia policia,
-                   List<Item> items, List<Ladron> ladrones, Mapa mapa) {
-        this(policia, items,ladrones,mapa, new Calendario(), new Random());
+            List<Item> items, List<Ladron> ladrones, Mapa mapa) {
+        this(policia, items, ladrones, mapa, new Calendario(), new Random());
     }
 
     /**
-     * Crea una misión para un policía, con un item y ladrón al azar, ruta al azar y ciudad según el ítem.
+     * Crea una misión para un policía, con un item y ladrón al azar, ruta al azar y
+     * ciudad según el ítem.
      * Para generar el ítem, ladrón y ruta usa el generador dado.
-     * Usado por constructor public por defecto (Policia,List<Item>,List<Ladron>,Mapa).
-     * @param policia El policía que realiza la investigación.
-     * @param items Listado de items para elegir uno como robado.
-     * @param ladrones Listado de ladrones para elegir uno.
-     * @param mapa El mapa de ciudades.
+     * Usado por constructor public por defecto
+     * (Policia,List<Item>,List<Ladron>,Mapa).
+     * 
+     * @param policia    El policía que realiza la investigación.
+     * @param items      Listado de items para elegir uno como robado.
+     * @param ladrones   Listado de ladrones para elegir uno.
+     * @param mapa       El mapa de ciudades.
      * @param calendario El calendario de tiempo del juego.
-     * @param random El generador de números aleatorios.
+     * @param random     El generador de números aleatorios.
      */
     private Mision(Policia policia,
-                   List<Item> items, List<Ladron> ladrones, Mapa mapa,
-                   Calendario calendario, Random random) {
-        this(policia, itemAlAzar(items,random),ladronAlAzar(ladrones,random),
+            List<Item> items, List<Ladron> ladrones, Mapa mapa,
+            Calendario calendario, Random random) {
+        this(policia, itemAlAzar(items, random), ladronAlAzar(ladrones, random),
                 new Computadora(ladrones), mapa,
                 calendario, random);
     }
 
     /**
      * Crea una misión con todos los datos dados, excepto ruta y ciudad de inicio.
-     * Usado por el constructor privado (policia, List<Item>,List<Ladron>, Mapa, Calendario, Random).
-     * @param policia El policía que realiza la investigación.
-     * @param item El elemento que fue robado.
-     * @param ladron El ladron que realizó el robo.
+     * Usado por el constructor privado (policia, List<Item>,List<Ladron>, Mapa,
+     * Calendario, Random).
+     * 
+     * @param policia     El policía que realiza la investigación.
+     * @param item        El elemento que fue robado.
+     * @param ladron      El ladron que realizó el robo.
      * @param computadora La computadora de Interpol.
-     * @param mapa El mapa de ciudades.
-     * @param calendario El calendario de tiempo del juego.
-     * @param random El generador de números aleatorios.
+     * @param mapa        El mapa de ciudades.
+     * @param calendario  El calendario de tiempo del juego.
+     * @param random      El generador de números aleatorios.
      */
-    public Mision(Policia policia, Item item,Ladron ladron,
-                Computadora computadora, Mapa mapa,
-                Calendario calendario, Random random)
-    {
-        this(policia, item, ladron, calcularRuta(item,mapa,random), item.getNombreCiudadDelRobo(),
-            computadora, mapa, calendario, random);
+    public Mision(Policia policia, Item item, Ladron ladron,
+            Computadora computadora, Mapa mapa,
+            Calendario calendario, Random random) {
+        this(policia, item, ladron, calcularRuta(item, mapa, random), item.getNombreCiudadDelRobo(),
+                computadora, mapa, calendario, random);
     }
-
-
 
     /**
      * Construye una nueva misión, con ruta y ciudad de inicio en base al ítem.
      * Usado en un test.
+     * 
      * @param unLadron
-     * @param unPolicia El policía que realiza la investigación.
-     * @param unItem El elemento que fue robado (el cual conoce la ciudad de inicio y largo de ruta).
-     * @param unMapa El mapa de ciudades.
+     * @param unPolicia    El policía que realiza la investigación.
+     * @param unItem       El elemento que fue robado (el cual conoce la ciudad de
+     *                     inicio y largo de ruta).
+     * @param unMapa       El mapa de ciudades.
      * @param unCalendario El calendario de tiempo del juego.
-     * @param random Un generador de números.
+     * @param random       Un generador de números.
      */
     public Mision(Ladron unLadron, Policia unPolicia, Item unItem,
-                  Computadora computadora, Mapa unMapa,
-                  Calendario unCalendario, Random random) {
-        this(unPolicia, unItem, unLadron, calcularRuta(unItem,unMapa, random), unItem.getNombreCiudadDelRobo(),
+            Computadora computadora, Mapa unMapa,
+            Calendario unCalendario, Random random) {
+        this(unPolicia, unItem, unLadron, calcularRuta(unItem, unMapa, random), unItem.getNombreCiudadDelRobo(),
                 computadora, unMapa,
                 unCalendario, random);
     }
@@ -154,10 +163,9 @@ public class Mision {
         return unItem.getRuta(unMapa, random);
     }
 
-
     public Ciudad viajarACiudad(Ciudad destino) {
         ciudadActual.desvisitar();
-        ciudadActual = mapa.viajar(policia,ciudadActual,destino);
+        ciudadActual = mapa.viajar(policia, ciudadActual, destino);
         return ciudadActual;
     }
 
@@ -166,11 +174,11 @@ public class Mision {
     }
 
     public void agregarDetalleLadron(String tipo, String valor) {
-        computadora.agregarDetalle(tipo,valor);
+        computadora.agregarDetalle(tipo, valor);
     }
 
     public void visitarEdificio(Edificio edificio) {
-        //edificio.visitar(policia);
+        // edificio.visitar(policia);
         policia.visitar(edificio);
     }
 
@@ -216,6 +224,10 @@ public class Mision {
 
     public List<Ladron> getSospechosos() {
         return computadora.buscarSospechosos();
+    }
+
+    public ObservableList<Ladron> getSospechososObservables() {
+        return FXCollections.observableList(computadora.buscarSospechosos());
     }
 
     public void oirOrden(ComputadoraListener oyente) {
