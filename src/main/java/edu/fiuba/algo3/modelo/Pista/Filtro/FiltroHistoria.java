8e64586eb5e79d2434e0e06d03bc73a6bfef4649
Matias Marcon <matiasmarcon6@gmail.com>
2021-12-22 20:46:40 -0300

    Creacion de nuevos filtros

---
+++ src/main/java/edu/fiuba/algo3/modelo/Pista/Filtro/FiltroHistoria.java
@@ -0,0 +1,22 @@
+package edu.fiuba.algo3.modelo.Pista.Filtro;
+
+import edu.fiuba.algo3.modelo.Pista.PistaCiudad;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FiltroHistoria implements IFiltroCiudad{
+
+    private static final ArrayList<String> tipos = new ArrayList<>(List.of(
+        "Geografia","Personajes Historicos","Miscelaneos","Religion","Arte","Gobernante"
+    ));
+
+    @Override
+    public List<PistaCiudad> filtrarPistas(Collection<PistaCiudad> sinFiltrar) {
+        return sinFiltrar.stream().filter(pista -> pista.esDeUnTipoDe(tipos))
+        .collect(Collectors.toList());
+    }
+    
+    
+}
