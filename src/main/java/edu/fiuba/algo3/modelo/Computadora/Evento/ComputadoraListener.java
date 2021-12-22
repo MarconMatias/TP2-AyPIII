823be0d35679e45ab77158430bae8c4b7537b858
desencuentro <->
2021-12-22 17:01:35 -0300

    Elevo volumen al cuadrado para que sea más bajo cuando es bajo, preparo para reemplazar evento por observable.

--- src/main/java/edu/fiuba/algo3/modelo/Computadora/Evento/ComputadoraListener.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo.Computadora.Evento;
-
-import java.util.EventListener;
-
-public interface ComputadoraListener extends EventListener {
-    void handle(ComputadoraEvento evento);
-}
