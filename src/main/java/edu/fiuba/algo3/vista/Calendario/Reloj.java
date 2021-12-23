f1e44f46fc92eb3e156705b9cbc4b1e4d5e98667
desencuentro <->
2021-12-22 22:15:18 -0300

    Creando Reloj

---
+++ src/main/java/edu/fiuba/algo3/vista/Calendario/Reloj.java
@@ -0,0 +1,22 @@
+package edu.fiuba.algo3.vista.Calendario;
+
+import edu.fiuba.algo3.componentes.Imagen.Imagen;
+import edu.fiuba.algo3.componentes.RelativoAImagen.RelativoAImagen;
+import edu.fiuba.algo3.modelo.Juego.Calendario;
+import javafx.beans.binding.ObjectExpression;
+import javafx.scene.image.Image;
+
+public class Reloj extends RelativoAImagen {
+    private static final Image fondo = new Image(Imagen.urlDesdeRecursos("Calendario/Reloj_fondo.png"));
+    //private final ImageView imageView;
+    private final ObjectExpression<Calendario> calendario;
+
+    public Reloj(ObjectExpression<Calendario> calendario) {
+        super(fondo);
+        /*imageView = new ImageView(fondo);
+        imageView.setFitWidth(fondo.getWidth());
+        imageView.setFitHeight(fondo.getHeight());
+        getChildren().setAll(imageView);*/
+        this.calendario = calendario;
+    }
+}
