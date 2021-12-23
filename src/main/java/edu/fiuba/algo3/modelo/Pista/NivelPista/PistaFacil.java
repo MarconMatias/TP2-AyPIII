0ef61ecfbd9fb8a58126f10897c9481a0601ae5e
desencuentro <->
2021-12-23 09:36:41 -0300

    PistaX debe coincidir también con la nueva PistaTodoNivel

--- src/main/java/edu/fiuba/algo3/modelo/Pista/NivelPista/PistaFacil.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pista/NivelPista/PistaFacil.java
@@ -4,6 +4,6 @@ public class PistaFacil extends NivelPista {
 
     @Override
     public boolean esEquivalente(NivelPista nivel) {
-        return nivel instanceof PistaFacil;
+        return (nivel instanceof PistaFacil) || (nivel instanceof PistaTodoNivel);
     }
 }
